3a7304a633a5cc99ff5c84048bf9f3cc7bcb892c
sebastienpro <spro@webgeoservices.com>
2020-04-19 10:07:23 +0200

    Add facebook pixel

--- deploy_proxisur.sh
+++ deploy_proxisur.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-aws s3 sync . s3://www.proxisur.fr --region "eu-west-1" --acl "public-read" --exclude ".git/*" --exclude ".idea/*"
+aws s3 sync . s3://www.proxisur.fr --region "eu-west-1" --acl "public-read" --exclude ".git/*" --exclude ".idea/*" --exclude "venv/*"
